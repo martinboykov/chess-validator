47d085066df14b1143a8e321848f125111f652d6
martinboykov <martinboykov@mail.bg>
2019-10-06 22:26:05 +0300

    constants

---
+++ src/util/constants.js
@@ -0,0 +1,33 @@
+const TYPES = {
+  default: '+',
+  king: 'k',
+  queen: 'q',
+  rook: 'r',
+  bishop: 'b',
+  knight: 'n',
+  pawn: 'p',
+};
+
+const COLORS = {
+  white: true,
+  black: false,
+};
+
+const PLAY_ORDER = {
+  white: true,
+  black: false,
+};
+
+
+const STATE = {
+  regular: 0,
+  check: 1,
+  checkmate: 2,
+};
+
+module.exports = {
+  TYPES,
+  COLORS,
+  PLAY_ORDER,
+  STATE,
+};
--- src/util/index.js
+++ src/util/index.js
@@ -1,39 +1,11 @@
 
 const { FileReader } = require('./file-reader');
 const { MOVEMENT_PATTERNS } = require('./move-patterns');
+const { ...CONSTANTS } = require('./constants');
 
-const TYPES = {
-	default: '+',
-	king: 'k',
-	queen: 'q',
-	rook: 'r',
-	bishop: 'b',
-	knight: 'n',
-	pawn: 'p',
-};
-
-const COLORS = {
-	white: true,
-	black: false,
-};
-
-const PLAY_ORDER = {
-	white: true,
-	black: false,
-};
-
-
-const STATE = {
-	regular: 0,
-	check: 1,
-	checkmate: 2,
-};
 
 module.exports = {
-	TYPES,
-	COLORS,
-	PLAY_ORDER,
-	STATE,
+	...CONSTANTS,
 	MOVEMENT_PATTERNS,
 	FileReader,
 };
